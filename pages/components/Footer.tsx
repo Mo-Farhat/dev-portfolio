cf826ce02f1930eb68f8a85ea76dc6bd7930978c
Mo-Farhat <mfarhat334@gmail.com>
2025-10-26 01:51:58 +0530

    second commit

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -64,7 +64,7 @@ const Footer = () => {
               whileHover={{ scale: 1.05 }}
               transition={{ duration: 0.2 }}
             >
-              Let's work together
+              Let&apos;s work together
             </motion.h3>
             <motion.p 
               className="text-gray-400"
